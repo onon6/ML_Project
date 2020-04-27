f14b680ff5d8d8eb0914a17b4acbdd42a092ae1d
Niels Guilliams <nielsguilliams@MacBook-Pro-van-Niels.local>
2020-04-27 13:15:11 +0200

    Pretty printing added

--- main.py
+++ main.py
@@ -15,22 +15,25 @@ from open_spiel.python.algorithms import tabular_qlearner
 
 FLAGS = flags.FLAGS
 
-flags.DEFINE_boolean("should_train", False, "Game should be trained")
-flags.DEFINE_boolean("should_play", False, "Game should be played")
+flags.DEFINE_boolean("should_train", True, "Game should be trained")
+flags.DEFINE_boolean("should_play", True, "Game should be played")
 
-flags.DEFINE_integer("num_episodes", int(1e4), "Number of train episodes.")
+# flags.DEFINE_integer("num_episodes", int(1e4), "Number of train episodes.")
+flags.DEFINE_integer("num_episodes", int(10), "Number of train episodes.")
 flags.DEFINE_boolean(
     "iteractive_play", True,
     "Whether to run an interactive play with the agent after training.")
 
 
-def command_line_action(time_step):
+def command_line_action(env, time_step):
     """Gets a valid action from the user on the command line."""
     current_player = time_step.observations["current_player"]
     legal_actions = time_step.observations["legal_actions"][current_player]
+
     action = -1
     while action not in legal_actions:
-        print("Choose an action from {}:".format(legal_actions))
+        pretty_print_actions(env, legal_actions)
+
         sys.stdout.flush()
         action_str = input()
         try:
@@ -60,15 +63,41 @@ def eval_against_random_bots(env, trained_agents, random_agents, num_episodes):
     return wins / num_episodes
 
 
+def pretty_print_actions(env, actions):
+    result = [[x, env.get_state.action_to_string(x)] for x in actions]
+
+    output = "\nChoose an action from {}:".format(actions)
+    for action in result:
+        output = output + "\n" + str(action[0]) + " : " + str(action[1])
+
+    logging.info(output)
+
+
+def pretty_print_state(env):
+    state = env.get_state
+    cards = str(state).split()[0:2]
+
+    def num_to_card(num):
+        switcher = {
+            0: "J",
+            1: "Q",
+            2: "K"
+        }
+
+        argument = int(num)
+        return switcher.get(argument, "Invalid card")
+
+    result = list(map(num_to_card, cards))
+    logging.info(
+        "\nPlayer  |  Card \n0       |  %s \n1       |  %s", result[0], result[1])
+
+
 def main(_):
     game = "kuhn_poker"
     num_players = 2
 
     env = rl_environment.Environment(game)
     num_actions = env.action_spec()["num_actions"]
-
-    print("ENV", dir(env))
-    print("TEST", env.get_state)
 
     agents = [
         tabular_qlearner.QLearner(player_id=idx, num_actions=num_actions)
@@ -105,29 +134,37 @@ def main(_):
 
     # 2. Play from the command line against the trained agent.
     if FLAGS.should_play:
-        human_player = 1
+        # Pretty print state
+        pretty_print_state(env)
+
+        player_1 = 0
         while True:
-            logging.info("You are playing as %s", "1" if human_player else "2")
             time_step = env.reset()
             while not time_step.last():
                 player_id = time_step.observations["current_player"]
-                if player_id == human_player:
-                    agent_out = agents[human_player].step(
+                if player_id == player_1:
+                    agent_out = agents[player_1].step(
                         time_step, is_evaluation=True)
 
-                    action = command_line_action(time_step)
+                    logging.info("Pick action for player %s", player_id)
+                    action = command_line_action(env, time_step)
                 else:
                     agent_out = agents[1 -
-                                       human_player].step(time_step, is_evaluation=True)
-                    action = agent_out.action
+                                       player_1].step(time_step, is_evaluation=True)
+
+                    logging.info("Pick action for player %s", player_id)
+                    action = command_line_action(env, time_step)
+
+                    # action = agent_out.action
+                    # logging.info("Agent action: %s", action)
                 time_step = env.step([action])
 
-            logging.info("Rewards: Human %s | Agent %s",
-                         time_step.rewards[human_player], time_step.rewards[1 - human_player])
+            logging.info("Rewards: Player_0 %s | Player_1 %s",
+                         time_step.rewards[player_1], time_step.rewards[1 - player_1])
             logging.info("End of game!")
 
             # Switch order of players
-            human_player = 1 - human_player
+            # human_player = 1 - human_player
 
 
 if __name__ == "__main__":
